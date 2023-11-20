af9e76ed0609b5f13af114cbbd517e2e5eaba567
20201064010008 <20201064010008@cang217516.ifrn.local>
2023-11-20 11:43:31 -0300

    Classe de animal aereo

--- src/Reinoanimal/Aereo.java
+++ src/Reinoanimal/Aereo.java
@@ -1,20 +1,39 @@
 package Reinoanimal;
 
+/**
+ * Classe responsável por gerar um animal aereo
+ * @author 20201064010008
+ *
+ */
 public class Aereo extends Animal {
 	private String altitude;
 	
+	/**
+	 * metodo que diz que o animal areo esta comendo
+	 */
 	public void comer() {
 		System.out.println("O animal  aereo  está comendo...");
 	}
 	
+	/**
+	 * metodo que diz que o animal aquatico esta voando
+	 */
+
 	public void voar() {
 		System.out.println("O animal está voando...");
 	}
 	
-	
+	/**
+	 * metodo que mostra o perfil do animal aereo
+	 */
 	public void perfil() {
 		System.out.println("Altitude" + this.altitude + "Especie:" + this.getEspecie() + "sexo:" + this.getSexo() +"Data:" + this.getData());
 	}
+	
+	/**
+	 * getters e setters
+	 * @return
+	 */
 
 	public String getAltitude() {
 		return altitude;
