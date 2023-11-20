61e02495f04199e627cbd113212a50f737f7cc72
20201064010008 <20201064010008@cang217516.ifrn.local>
2023-11-20 11:43:49 -0300

    Classe  Animal

--- src/Reinoanimal/Animal.java
+++ src/Reinoanimal/Animal.java
@@ -1,24 +1,43 @@
 package Reinoanimal;
 
-public class Animal {
+/**
+ * Essa classe é responsável por gerar um animal
+ * @author 20201064010008
+ *
+ */
 
+public class Animal {
+	
 	private String especie;
 	private String sexo;
 	private String data;
 	private static int contador;
 	
+	/**
+	 * Este metodo é responsável por contar a quantidade de animais cadastrados 
+	 */
 	public Animal() {
 		setContador();
 	}
 	
+	/**
+	 * metodo que diz que o animal esta comendo
+	 */
 	public void comer() {
 		System.out.println("O animal está comendo...");
 	}
 	
+	/**
+	 * metodo que vai exibir o perfil do animal,como: especie,sexo e data
+	 */
 	public void perfil() {
 		System.out.println("Especie:" + this.especie + "sexo:" + this.sexo +"Data:" + this.data);
 	}
 	
+	/**
+	 * getters e setters
+	 * @return
+	 */
 	
 	public String getEspecie() {
 		return especie;
