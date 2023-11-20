972eaf09c7f4d3ce7ac8ab9e45455381099f8d68
20201064010008 <20201064010008@cang217516.ifrn.local>
2023-11-20 11:45:00 -0300

    Classe de animal terrestre

--- src/Reinoanimal/Terrestre.java
+++ src/Reinoanimal/Terrestre.java
@@ -1,24 +1,40 @@
 package Reinoanimal;
 
+/**
+ * Classe responsável por gerar um animal terrestre
+ * @author 20201064010008
+ *
+ */
 public class Terrestre extends Animal{
 
 	private String regiao;
 	private boolean pelo;
 	
-
+	/**
+	 * metodo que diz que o animal terrestre esta comendo
+	 */
 	public void comer() {
 		System.out.println("O animal  Terrestre está comendo...");
 	}
-	
+	/**
+	 * metodo que diz que o animal aquatico esta se movendo
+	 */
+
 	public void mover() {
 		System.out.println("O animal está se movendo...");
 	}
 	
-	
+	/**
+	 * metodo que mostra o perfil do animal terrestre
+	 */
 	public void perfil() {
 		System.out.println("Tipo de região:" + this.regiao + "pelo:" + this.pelo  + "Especie:" + this.getEspecie() + "sexo:" + this.getSexo() +"Data:" + this.getData());
 	}
 
+	/**
+	 * getters e setters
+	 * @return
+	 */
 	public String getRegiao() {
 		return regiao;
 	}
